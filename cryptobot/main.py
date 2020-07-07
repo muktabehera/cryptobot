202443da9f8e138f4104092df2dcbec8eeeaa487
Satya Mohapatra <mohapsat@gmail.com>
2020-07-06 19:46:02 -0700

    wip market sell

--- cryptobot/main.py
+++ cryptobot/main.py
@@ -28,7 +28,7 @@ def get_response(payload, api_method, resource):
     api_subaccountId = ""   # empty string
     # api_method = "GET"  # GET, POST
     api_url = f"{api_endpoint}/{resource}"
-    logging.info(api_url)
+    # logging.info(api_url)
 
     preSign = api_ts + api_url + api_method + api_contentHash
     api_signature = hmac.new(api_secret.encode(), preSign.encode(), hashlib.sha512).hexdigest()
@@ -42,7 +42,7 @@ def get_response(payload, api_method, resource):
     }
 
     if api_method == "POST":
-        response = requests.post(url=api_url, data=json.dumps(payload), headers=headers).json()
+        response = requests.post(url=api_url, data=payload, headers=headers).json()
     elif api_method == "DELETE":
         response = requests.delete(url=api_url, headers=headers).json()
     else:
@@ -51,28 +51,40 @@ def get_response(payload, api_method, resource):
     return response
 
 
-def get_balance(pair):
-    resource = f"balances/{pair}"
+def get_balance(currencySymbol):
+    resource = f"balances/{currencySymbol}"
     payload = ""
     api_method = "GET"
     balances = get_response(payload, api_method, resource)
-    logging.info(balances)
+    return balances
 
 
 def get_total_balances():
     resource = "balances"
     payload = ""
     api_method = "GET"
-    balances = get_response(payload, api_method, resource)
-    logging.info(balances)
+    total_balances = get_response(payload, api_method, resource)
+    # logging.info(total_balances)
+    return total_balances
 
 
-def get_open_orders(pair):
-    resource = f"orders/open?{urllib.parse.urlencode({'marketSymbol': pair})}"
+def get_open_orders(marketSymbol):
+    resource = f"orders/open?{urllib.parse.urlencode({'marketSymbol': marketSymbol})}"
     payload = ""
     api_method = "GET"
     openorders = get_response(payload, api_method, resource)
-    logging.info(openorders)
+    # logging.info(type(openorders)) -- list
+    # logging.info(openorders)
+    return openorders
+
+
+def get_closed_orders(marketSymbol):
+    resource = f"orders/closed?{urllib.parse.urlencode({'marketSymbol': marketSymbol})}"
+    payload = ""
+    api_method = "GET"
+    closedorders = get_response(payload, api_method, resource)
+    # logging.info(closedorders)
+    return closedorders
 
 
 def get_order_details(orderid):
@@ -80,7 +92,8 @@ def get_order_details(orderid):
     payload = ""
     api_method = "GET"
     orderDetails = get_response(payload, api_method, resource)
-    logging.info(orderDetails)
+    # logging.info(orderDetails)
+    return orderDetails
 
 
 def get_order_executions(orderid):
@@ -88,8 +101,8 @@ def get_order_executions(orderid):
     payload = ""
     api_method = "GET"
     orderexecutions = get_response(payload, api_method, resource)
-    logging.info(orderexecutions)
-
+    # logging.info(orderexecutions)
+    return orderexecutions
 
 
 def cancel_order(orderid):
@@ -105,15 +118,20 @@ def get_order_history(pair):
     payload = ""
     api_method = "GET"
     orderhist = get_response(payload, api_method, resource)
-    logging.info(orderhist)
+    # logging.info(orderhist)
+    return orderhist
 
 
-def buy(marketSymbol, qty):
+def buy(marketsymbol, qty):
+
     resource = "orders"
-    payload = f"{'marketSymbol': {marketSymbol}, 'direction': 'BUY', 'type': 'MARKET', 'timeInForce': 'GOOD_TIL_CANCELLED', 'quantity': {qty}}"
+    payload = f"{{'marketSymbol': '{marketsymbol}', 'direction': 'BUY', 'type': 'MARKET', 'timeInForce': 'IMMEDIATE_OR_CANCEL', 'quantity': {qty}}}"
+    logging.info(f"Buy Payload = {payload}")
     api_method = "POST"
-    buylimit = get_response(payload, api_method, resource)
-    logging.info(buylimit)
+    buy_order_details = get_response(payload, api_method, resource)
+    logging.info(buy_order_details)
+    return buy_order_details
+    # {'id': 'e5a52228-7c79-458a-b41c-6e40dd9f0988', 'marketSymbol': 'BTC-USD', 'direction': 'BUY', 'type': 'MARKET', 'quantity': '0.01027115', 'timeInForce': 'IMMEDIATE_OR_CANCEL', 'fillQuantity': '0.01025849', 'commission': '0.19050467', 'proceeds': '95.21448268', 'status': 'CLOSED', 'createdAt': '2020-07-07T02:29:33.76Z', 'updatedAt': '2020-07-07T02:29:33.76Z', 'closedAt': '2020-07-07T02:29:33.76Z'}
 
 
 def sell(marketSymbol, qty):
@@ -128,8 +146,8 @@ def sell(marketSymbol, qty):
     resource = "orders"
     payload = f"{'marketSymbol': {marketSymbol}, 'direction': 'BUY', 'type': 'MARKET', 'timeInForce': 'GOOD_TIL_CANCELLED'}"
     api_method = "POST"
-    selllimit = get_response(payload, api_method, resource)
-    logging.info(selllimit)
+    sell = get_response(payload, api_method, resource)
+    logging.info(sell)
 
 
 def get_orderbook(marketSymbol):
@@ -137,7 +155,17 @@ def get_orderbook(marketSymbol):
     payload = ""
     api_method = "GET"
     orderbook = get_response(payload, api_method, resource)
-    logging.info(orderbook)
+    # logging.info(orderbook)
+    return orderbook
+
+
+def get_ticker(marketSymbol):
+    resource = f"markets/{marketSymbol}/ticker"
+    payload = ""
+    api_method = "GET"
+    ticker = get_response(payload, api_method, resource)
+    # logging.info(ticker)
+    return ticker
 
 
 def get_candles(marketSymbol, candleInterval):
@@ -151,30 +179,127 @@ def get_candles(marketSymbol, candleInterval):
     r_candles = get_response(payload, api_method, resource)
     # logging.info(r_candles)
 
-    candles = list()
+    close = list()
+    vol = list()
+
     for i in r_candles:
-        candles.append(i['close'])
+        close.append(i['close'])
+        vol.append(i['volume'])
 
     # logging.info(candles)
     # logging.info(len(candles))
-    return candles
-
-
+    return {'close': close, 'vol': vol}
 
 
 if __name__ == '__main__':
 
-    # get_balance('USD')
+    marketsymbol = 'BTC-USD'
+    currencysymbol = 'BTC'
+
+    commission = 0.04           # .20% each side, total .40%
+    slippage_buffer = 0.01
+
+    sell_signal = False
+    buy_signal = False
+
+    qty = 0.000                 # float, 3 places of decimal
+    buy_price = None
+    balance_gt_0 = False    # USD balance greater than 0
+
+    open_order_exists = False   # open order exists
+    order_exists = False        # order exists
+    offer_exists = False        # ask price and vol exist  and account for slippage and commission
+
+    ## --> Trading Rules:
+        # Only 1 open order at a time
+        # Open an order only if previous order has been closed
+        # Always Buy then sell, never sell first
+
+    ## --> Trade Flow
+        # check if open order exists, if yes, skip
+        # if no, check balance of symbol
+        # set order_exists = True
+
+    logging.info("checking for open order")
+    open_order = get_open_orders(marketsymbol)
+    logging.info(open_order)
+    if open_order:
+        open_order_exists = True
+        qty = open_order['quantity']
+        logging.info(f"open order exists for {qty} {marketsymbol}")
+    else:
+        logging.info("No open order found")
+        # get current account balance
+        balance = get_balance(currencysymbol)
+        # logging.info(f"Balance = {order}")
+        if float(balance['available']) > 0.000:
+            balance_gt_0 = True
+            qty = float(balance['available'])
+            logging.info(f"{qty} units of {currencysymbol} available to sell")
+            # get buy price
+            # since there's balance get most recent buy order id
+            recent_orders = get_closed_orders(marketsymbol)
+
+            if recent_orders and recent_orders[0]['direction'] == 'BUY':
+
+                logging.info(recent_orders[0])
+                recent_buy_order_id = recent_orders[0]['id']
+                recent_buy_order_commission = recent_orders[0]['commission']
+                recent_buy_order_createdAt = recent_orders[0]['createdAt']
+                recent_buy_order_closedAt = recent_orders[0]['closedAt']
+                recent_buy_order_proceeds = recent_orders[0]['proceeds']
+                # get buy price and qty filled from that order
+                qty = recent_orders[0]['fillQuantity']
+                buy_price = (recent_buy_order_proceeds + recent_buy_order_commission) / qty
+
+                # check if current ask_price
+
+        else:
+            logging.info(f"0 units of {currencysymbol} available to sell")
+
+
+
+    ticker = get_ticker(marketsymbol)
+    # {'symbol': 'XRP-USD', 'lastTradeRate': '0.17600000', 'bidRate': '0.17587000', 'askRate': '0.17644000'}
+    ask_rate = float(ticker['askRate'])
+    logging.info(f"{currencysymbol} ask_rate (offer) = {ask_rate}")
+
+
+
+    # if ask_rate >=
+
+    # sell_signal
+        # if order exists
+        # if ask (offer) >= buy_price + (buy_price * commission) + slippage
+
+    sell_signal = order_exists and offer_exists
+    
+    if sell_signal:
+        logging.info(f"sell {qty} at market")
+        # sell(marketsymbol, qty)
+
+    # logging.info("cancelling order 'd91ba2c1-3ec8-428c-bc48-0073483e0734'")
+    # cancel_order('d91ba2c1-3ec8-428c-bc48-0073483e0734')
+
+    # get_balance('BTC')  # 'USD'
     # get_total_balances()
     # get_open_orders('XRP-USD')
-    # get_order_history('BTC-USD')
+    # logging.info(get_order_history('BTC-USD'))
     # get_order_details('5d3fc794-8f32-42e3-850c-ecd642b5b763') # orderid
     # get_order_executions('5d3fc794-8f32-42e3-850c-ecd642b5b763')
     # cancel_order('5d3fc794-8f32-42e3-850c-ecd642b5b763')    # {'code': 'ORDER_NOT_OPEN'}
-    # buy_market('XRP-USD', '100')
+    # buy('BTC-USD', 0.01027115)
     # sell_market('XRP-USD', '100')
     # get_orderbook('XRP-USD')
-    get_candles('XRP-USD', 'DAY_1')
+    # get_ticker('XRP-USD')
+    # get_closed_orders('BTC-USD')
+    # get_open_orders('BTC-USD')
+    # logging.info(get_candles('XRP-USD', 'DAY_1')['close'])
+    # logging.info(get_candles('XRP-USD', 'DAY_1')['vol'])
+
+    # Strategy
+    # 50, 200 SMA (buy)
+
 
     pass
 
